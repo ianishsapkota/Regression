47fb6abae519a5045670c85927ef17d32a4e5e6a
Anish Sapkota <anishsapkota926@gmail.com>
2025-11-30 16:40:52 +0545

    Train the model and perform prediction on test set

--- Multiple Linear Regression/main.py
+++ Multiple Linear Regression/main.py
@@ -19,5 +19,9 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, rando
 
 #model training
 mlr = LinearRegression()
-mlr.fit(y_train,y_train)
+mlr.fit(x_train,y_train)
 
+#predictioin
+y_pred = mlr.predict(x_test)
+np.set_printoptions(precision = 2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),axis=1))
