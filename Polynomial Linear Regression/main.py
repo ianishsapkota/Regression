b9f1a773f0f5413d56d506c629ba8751688db4a3
Anish Sapkota <anishsapkota926@gmail.com>
2025-12-01 17:37:35 +0545

    train polynomial regression model and visualize

--- Polynomial Linear Regression/main.py
+++ Polynomial Linear Regression/main.py
@@ -12,10 +12,23 @@ y = df.iloc[:,-1].values
 lr = LinearRegression()
 lr.fit(x, y)
 
-# Visualize linear regression results
-plt.scatter(x, y, color='red')
-plt.plot(x, lr.predict(x), color='blue') 
+#Visualize linear regression results
+plt.scatter(x, y, color ='red')
+plt.plot(x, lr.predict(x), color ='blue') 
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Linear Regression')
+
+#Train polynomial regression
+pr = PolynomialFeatures(degree=4)
+x_poly = pr.fit_transform(x)
+lr_2 = LinearRegression()
+lr_2.fit(x_poly,y)
+
+#Visualize polynomial regerssion model
+plt.scatter(x, y, color='red')
+plt.plot(x, lr_2.predict(x_poly), color='blue')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Polynomial Regression')
 plt.show()
